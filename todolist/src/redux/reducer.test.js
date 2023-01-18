0a366a8dd2f65e1ab5e9ec118526537bb830fc53
kim-seung-hun <rlatmdgns16@naver.com>
2023-01-18 19:43:19 +0900

    prac

--- todolist/src/redux/reducer.test.js
+++ todolist/src/redux/reducer.test.js
@@ -4,6 +4,7 @@ import { setTasks } from "../actions";
 
 import tasks from "../../fixtures/tasks";
 
+// tdd 연습
 describe("reducer", () => {
   describe("setTasks", () => {
     it("change tasks array", () => {
